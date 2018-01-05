77b44ba56911e32f49bc043f2054d6b703067558
githup.jun.li <976991218@qq.com>
2018-01-05 14:51:24 +0800

    删除class文件

--- src/com/jun/test/Client.java
+++ src/com/jun/test/Client.java
@@ -1,6 +1,7 @@
 package com.jun.test;
 
 public class Client {
+	/*	测试抽象工厂模式       */	
 	public static void main(String[]args){
         ComputerEngineer cf = new ComputerEngineer();
         cf.makeComputer(1,1);
